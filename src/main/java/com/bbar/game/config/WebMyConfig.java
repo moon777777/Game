bca9589dcb525ca1ff31a486a17b90e4df17975b
moon777 <myj2139@gmail.com>
2025-01-17 03:43:38 +0900

    [문영주] - Interceptor 기능
    #20

--- src/main/java/com/bbar/game/config/WebMyConfig.java
+++ src/main/java/com/bbar/game/config/WebMyConfig.java
@@ -1,10 +1,12 @@
 package com.bbar.game.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.bbar.game.common.FileManager;
+import com.bbar.game.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMyConfig implements WebMvcConfigurer {
@@ -13,6 +15,15 @@ public class WebMyConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(new PermissionInterceptor())
+		.addPathPatterns("/**")
+		.excludePathPatterns("/user/logout", "/static/**", "/images/**", "/post/list-view", "/post/calendar-view"
+				,"/post/calendar-data", "/post/detail-view/{id}");
 	}
 	
 	
---
+++ src/main/java/com/bbar/game/interceptor/PermissionInterceptor.java
@@ -0,0 +1,40 @@
+package com.bbar.game.interceptor;
+
+import java.io.IOException;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class PermissionInterceptor implements HandlerInterceptor {
+	
+	@Override
+	public boolean preHandle(
+			HttpServletRequest request
+			, HttpServletResponse response
+			, Object handler) throws IOException {
+		
+		HttpSession session = request.getSession();
+		
+		Integer userId = (Integer)session.getAttribute("userId");
+		
+		String uri = request.getRequestURI();
+		
+		if(userId == null) {
+			if(uri.startsWith("/post")) {
+				
+				response.sendRedirect("/user/login-view");
+				return false;
+			}
+		} else {
+			if(uri.startsWith("/user")) {
+				response.sendRedirect("/post/list-view");
+				return false;
+			}
+		}
+	return true;
+	}
+
+}
